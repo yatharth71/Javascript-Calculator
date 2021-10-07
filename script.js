98a77f27a84658ed6a4730853a8de84090778968
Yatharth Singh <82080455+yatharth71@users.noreply.github.com>
2021-10-07 16:19:28 +0530

    added the equals to button

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ let multiply = document.getElementById("multiply");
 let number1 = document.getElementById("text1");
 let number2 = document.getElementById("text2");
 let result = document.getElementById("result");
-result.innerHTML = '<h1 id="result">Result : 0</h1>'
+let equal = document.getElementById("equal")
 
+result.innerHTML = '<h1 id="result">Result : 0</h1>';
 // Writing the logic
 add.addEventListener("click", e => {
     r = parseInt(number1.value) + parseInt(number2.value);
